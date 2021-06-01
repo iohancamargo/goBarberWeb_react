bf81462218dbe6f12c42ff28eeea4a3bb3441774
06058395909 <iohan.camargo@sevensys.tech>
2021-06-01 00:21:57 -0300

    Inserindo conceito de interfaces/ adc verify jwt

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -13,7 +14,8 @@ const Routes: React.FC = () => (
     <Route path="/signup" exact component={SignUp} />
     <Route path="/forgot-password" exact component={ForgotPassword} />
     <Route path="/reset-password" exact component={ResetPassword} />
-    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+
+    <Route path="/profile" exact component={Profile} isPrivate />
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
